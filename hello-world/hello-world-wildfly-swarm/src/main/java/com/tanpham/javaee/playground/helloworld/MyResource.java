779b10eb716c25790cbcba3b0963f47a3824f124
phamngoctan <pnt.dtqn1991@gmail.com>
2019-11-04 18:35:07 +0700

    Initial commit for hello world project with wildfly swarm

---
+++ hello-world/hello-world-wildfly-swarm/src/main/java/com/tanpham/javaee/playground/helloworld/MyResource.java
@@ -0,0 +1,21 @@
+package com.tanpham.javaee.playground.helloworld;
+
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+@Path("/endpoint")
+public class MyResource {
+	
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getWithQuery(@DefaultValue("bar") @QueryParam("search") String searchQueryParamValue) {
+        return Response.status(Status.ACCEPTED).entity(searchQueryParamValue).build();
+    }
+    
+}
