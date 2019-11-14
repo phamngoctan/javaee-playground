803119ccedda0c8f49f3fd7ee915af9ea6d94288
Tan Pham <pnt.dtqn1991@gmail.com>
2019-11-14 07:23:06 +0700

    Initial commit for thorntail playground

---
+++ thorntail-playground/src/main/java/com/tanpham/playground/javaee/thorntailplayground/rest/RestApplication.java
@@ -0,0 +1,8 @@
+package com.tanpham.playground.javaee.thorntailplayground.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class RestApplication extends Application {
+}
